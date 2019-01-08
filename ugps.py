eecafc642b8965027e507076221ca0c306a90f21
Adam Doan <adam.c.doan@gmail.com>
2019-01-08 16:17:53 -0500

    Stubbed out function to get dois from web of science.

--- ugps.py
+++ ugps.py
@@ -2,20 +2,47 @@ from wos import WosClient
 import wos.utils
 
 import secrets
+from models import *
+
 
 # with WosClient(secrets.WOS_USER, secrets.WOS_PASS) as client:
 #     print(wos.utils.query(client, 'OG="University of Guelph"'))
 
-from models import *
-
 
 def init_db():
+    """
+    Initialize the database, creating the tables if not already present
+
+    :return: A peewee database object
+    """
     from peewee import SqliteDatabase
 
     db = SqliteDatabase('ugps.db')
     with db:
         db.create_tables([Author, Publisher, Journal, Article, Authored])
+
     return db
 
 
+def get_dois():
+    """
+    Get a list of UG DOIs from the Web of Science API.
+    :return: a list of DOIs
+    """
+
+    # We're just stubbing this out for now, because WOS hasn't approved my developer application yet.
+    dois = ["10.1016/j.tourman.2018.10.025",
+            "10.1098/rspb.2002.2218",
+            "10.1038/nature11234",
+            "10.1021/cr00104a001",
+            "10.1111/j.1471-8286.2006.01678.x"]
+
+    return dois
+
+
 init_db()
+
+dois = get_dois()
+
+for doi in dois:
+    pass
