2eead1c1e179bb9f6c79b362cd07ea84b16a6c01
Adam Doan <adam.c.doan@gmail.com>
2019-01-08 13:33:47 -0500

    Now ignoring secrets, and with secrets template file.

---
+++ secrets.template.py
@@ -0,0 +1,6 @@
+# Sign up for Web of Science credentials at https://developer.clarivate.com
+# Before you put all your secret data into this file, make a copy and rename it to secrets.py.
+# Also, make sure that your credentials aren't added to source control!
+
+WOS_USER = ''
+WOS_PASS = ''
