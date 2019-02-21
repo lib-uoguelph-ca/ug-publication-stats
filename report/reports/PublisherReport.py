e422b565c460136c23f05237a9c3b276f23f45b7
Adam Doan <adam.c.doan@gmail.com>
2019-02-21 15:55:17 -0500

    Now with publisher report.

---
+++ report/reports/PublisherReport.py
@@ -0,0 +1,62 @@
+from report.reporting import Report
+from models import db_file_name
+
+from csv import DictWriter
+import sqlite3
+
+
+class PublisherReport(Report):
+
+    name = "Publisher Report"
+    description = "OA article counts by publisher"
+
+    mapping = {
+        'publisher': lambda record: record['name'],
+        'oa count': lambda record: record['oa_count'],
+        'hybrid count': lambda record: record['hybrid_count'],
+        'bronze count': lambda record: record['bronze_count'],
+        'green count': lambda record: record['green_count'],
+    }
+
+    def __init__(self):
+        conn = sqlite3.connect(db_file_name)
+        conn.row_factory = sqlite3.Row
+
+        self.conn = conn
+        self.cursor = conn.cursor()
+
+    def __del__(self):
+        self.conn.close()
+
+    def do_query(self):
+
+        results = self.cursor.execute(
+            """
+            SELECT p.name as name, 
+                sum(a.oa) as oa_count, 
+                sum(a.hybrid) as hybrid_count, 
+                sum(a.bronze) as bronze_count, 
+                sum(a.self_archived) as green_count 
+            FROM article a 
+            LEFT JOIN publisher p on a.publisher_id = p.id
+            GROUP BY a.publisher_id
+            """)
+
+        return results.fetchall()
+
+    def run(self, outfile=None):
+
+        print(f"Running {self.name}")
+
+        if not outfile:
+            file_name = self._make_file_name()
+            outfile = f'./output/{file_name}.csv'
+
+        with open(outfile, 'w', newline='') as csvfile:
+            writer = DictWriter(csvfile, fieldnames=self.mapping.keys())
+            writer.writeheader()
+
+            results = self.do_query()
+
+            for result in results:
+                writer.writerow(self.get_values(result))
