99fa7564c1b732f1f8c735a275d48ce25c7643b4
Adam Doan <adam.c.doan@gmail.com>
2019-03-11 13:20:52 -0400

    Journal report now includes average APC cost and an APC cost estimation.

--- report/reports/JournalReport.py
+++ report/reports/JournalReport.py
@@ -17,6 +17,8 @@ class JournalReport(Report):
         'bronze count': lambda record: record['bronze_count'],
         'green count': lambda record: record['green_count'],
         'total count': lambda record: record['total_count'],
+        'average apc cost': lambda record: record['average_apc'],
+        'estimated apc cost': None
     }
 
     def __init__(self):
@@ -25,6 +27,8 @@ class JournalReport(Report):
 
         self.conn = conn
         self.cursor = conn.cursor()
+
+        self.mapping['estimated apc cost'] = self.estimate_cost
 
     def __del__(self):
         self.conn.close()
@@ -38,7 +42,8 @@ class JournalReport(Report):
                 sum(a.hybrid) as hybrid_count, 
                 sum(a.bronze) as bronze_count, 
                 sum(a.self_archived) as green_count,
-                count(a.id) as total_count
+                count(a.id) as total_count,
+                j.average_apc as average_apc
             FROM article a 
             LEFT JOIN journal j on a.journal_id = j.id
             GROUP BY a.journal_id
@@ -64,3 +69,17 @@ class JournalReport(Report):
 
             for result in results:
                 writer.writerow(self.get_values(result))
+
+    def estimate_cost(self, record):
+        """
+        Use the average apc cost value to calculate an estimation of the cost paid to a publisher.
+        :param record: A record from the query above counting publications of each time for a publisher.
+        :return: Float - An estimated cost paid to the publisher.
+        """
+
+        if record['average_apc']:
+            record_count = record['oa_count'] + record['hybrid_count']
+            estimated_cost = record_count * record['average_apc']
+            return estimated_cost
+
+        return None
